5dce3ef60dda1c0d4de9f8c94395028cf5011595
alek-jaan Tsern <55093450+Winlanto@users.noreply.github.com>
2022-02-14 12:59:32 +0200

    Tund1

---
+++ 01_funktsioon/Tervitus2a.java
@@ -0,0 +1,10 @@
+ public class Tervitus2a{
+	public static void main(String[] arg){
+		if(arg.length>1){
+		System.out.println("Tere, "+arg[0]+" ja "+arg[1]);	
+		} else {
+			System.out.println("Pole kahte nime" );
+		}
+		//kui nimesid on vahemalt kaks, siis tervitage kahte esimest nime
+	}
+}
